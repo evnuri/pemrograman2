87804f502a446eb692d1d3ac113b42d70a9b9fa7
Smart Campus <demita.evnuri@gmail.com>
2019-01-24 23:29:15 +0700

    cariLuasPersegiPanjang dengan input panjang dan lebar

---
+++ HitungLuas.java
@@ -0,0 +1,12 @@
+class NamaProgram {
+    public static void main(){
+        
+    }
+	public static void cariLuasPersegiPanjang(String[]args){
+        int panjang;
+		int lebar;
+		
+		panjang = scan.nextInt();
+		 lebar = scan.nextInt();
+    }
+}
